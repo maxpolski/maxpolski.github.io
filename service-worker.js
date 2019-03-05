5038633d1da21390ad3ed902c2106fecf6d24eee
Maksym Korabelskyi <maksym.korabelskyi@devico.io>
2019-03-05 22:08:56 +0200

    change style + fix logic

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a2d587a97562b1c11454ff4787356fd.js"
+  "/precache-manifest.03e6f011c42dc6e9d932977934243add.js"
 );
 
 workbox.clientsClaim();
